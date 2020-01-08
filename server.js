41be4fede5d8112318e734f266e9945ad4d9fdee
Israel Wilson <israelsjwilson@gmail.com>
2020-01-07 22:32:28 -0600

    Adding http route methods for express api

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const next = require('next')
 const bodyParser = require('body-parser')
 const nodemailer = require('nodemailer')
+const mysql = require('mysql')
 const config = require('./next.config.js')
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -45,7 +46,53 @@ app.prepare().then(() => {
     })
   })
 
+  // Handle Routes Here
   server.get('*', (req, res) => {
+    return handle(req, res)
+  })
+
+  // Do they have access
+  server.get('/access/:id', (req, res) => {
+    return handle(req, res)
+  })
+
+  // Toggle voting access
+  server.put('/toggleAccess', (req, res) => {
+    return handle(req, res)
+  })
+
+  // Is voting open
+  server.get('/open', (req, res) => {
+    return handle(req, res)
+  })
+
+  // Toggle open state
+  server.put('/toggleOpen', (req, res) => {
+    return handle(req, res)
+  })
+
+  // Get all votes
+  server.get('/tally', (req, res) => {
+    return handle(req, res)
+  })
+
+  // Update votes
+  server.put('/update', (req, res) => {
+    return handle(req, res)
+  })
+
+  // Delete candidate
+  server.delete('/delete/:name', (req, res) => {
+    return handle(req, res)
+  })
+
+  // Add candidate
+  server.post('/add/:name', (req, res) => {
+    return handle(req, res)
+  })
+
+  // Remove all candidates
+  server.delete('/nuke', (req, res) => {
     return handle(req, res)
   })
 
