d6bd8295600fd14f5188c99ebae7adc615d847a2
Israel Wilson <israelsjwilson@gmail.com>
2020-01-11 18:03:05 -0600

    Testing sign in button

--- pages/Home.js
+++ pages/Home.js
@@ -6,7 +6,8 @@ export default class Home extends React.Component {
     super(props);
     this.state = {
       live: false,
-      candidates: []
+      candidates: [],
+      userID: ""
     };
   }
 
@@ -15,14 +16,46 @@ export default class Home extends React.Component {
   }
 
   login() {
+    if(this.state.userID == ""){
+      return;
+    }
+    const userID = this.state.userID;
 
+    fetch('/access/' + userID, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        userID: userID
+      })
+    })
+    .then((res) => res.text())
+    .then((res) => {
+      console.log('Contact form response: ', res);
+    })
+    .catch((err) => {
+      console.log('Contact form error: ', err);
+    })
+
+  }
+
+  change(event) {
+    const target = event.target;
+    const name = target.name;
+    const value = target.value;
+
+    this.setState({[name]: value});
   }
 
   render() {
     return (
       <Container>
-          <input />
-          <button>Hello</button>
+        <form onClick={login.bind(this)}>
+          <input type="text" name="userID" value={this.state.userID} onChange={change.bind(this)}/>
+          <button type="button">Sign In</button>
+        </form>
         <style jsx>{`
 
         `}</style>
