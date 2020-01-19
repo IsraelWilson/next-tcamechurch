a029ca09d6c132fab5ad96ac233ab20d01e04929
Israel Wilson <israelsjwilson@gmail.com>
2020-01-19 16:29:03 -0600

    Adding methods and elements to the Vote and Confirm pages.

--- pages/Confirm.js
+++ pages/Confirm.js
@@ -5,14 +5,44 @@ export default class Confirm extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      candidates: []
     };
+  }
+
+  getColumn = (buttonArr) => {
+    return <Column>{buttonArr}</Column>;
+  }
+
+  getColumns = (candidates) => {
+    let buttons = this.getButtons(candidates);;
+    let column = [];
+    let columns = [];
+
+    for(let i = 0; i < buttons.length; i++) {
+      column.push(buttons[i]);
+      if(i % 10 == 0 && i != 0 || i == buttons.length - 1) {
+        columns.push(this.getColumn(column));
+        column = [];
+      }
+    }
+
+    return columns;
+  }
+
+  getRow = (candidates) => {
+    return <Row>{this.getColumns(candidates)}</Row>;
+  }
+
+  getButtons = (candidates) => {
+    let buttons = [];
+    candidates.map((candidate) => buttons.push(<button type="button">{candidate.trustee_name}: <label>{candidate.vote_num}</label></button>));
+    return buttons;
   }
 
   render() {
     return (
       <Container>
-        <input />
-        <button></button>
+        {this.getRow(this.props.selection)}
         <style jsx>{`
 
         `}</style>
--- pages/Vote.js
+++ pages/Vote.js
@@ -1,18 +1,79 @@
 import React from 'react'
 import Container from '../components/Container.js'
+import Row from '../components/Row'
+import Column from '../components/Column'
 
 export default class Vote extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      candidates: [],
+      selection: 0
     };
+  }
+
+  componentDidMount = () => {
+    this.tally()
+  }
+
+  tally = () => {
+    fetch('/tally')
+    .then((res) => res.json())
+    .then((res) => {
+      this.setState({candidates: res});
+    })
+    .catch((err) => { console.log(err) })
+  }
+
+  getColumn = (buttonArr) => {
+    return <Column>{buttonArr}</Column>;
+  }
+
+  getColumns = (candidates) => {
+    let buttons = this.getButtons(candidates);;
+    let column = [];
+    let columns = [];
+
+    for(let i = 0; i < buttons.length; i++) {
+      column.push(buttons[i]);
+      if(i % 10 == 0 && i != 0 || i == buttons.length - 1) {
+        columns.push(this.getColumn(column));
+        column = [];
+      }
+    }
+
+    return columns;
+  }
+
+  getRow = (candidates) => {
+    return <Row>{this.getColumns(candidates)}</Row>;
+  }
+
+  getButtons = (candidates) => {
+    let buttons = [];
+    candidates.map((candidate) => buttons.push(<button type="button">{candidate.trustee_name}: <label>{candidate.vote_num}</label></button>));
+    return buttons;
+  }
+
+  submit = () => {
+    if(this.state.selection != 10){
+      return;
+    }
+
+    this.props.getPage("confirm");
+
   }
 
   render = () => {
     return (
       <Container>
-        <input />
-        <button>Vote</button>
+        {this.getRow(this.state.candidates)}
+        <form onClick={this.submit.bind(this)}>
+          <Row>
+            <label>{this.state.selection}</label>
+            <button type="button">Submit</button>
+          </Row>
+        </form>
         <style jsx>{`
 
         `}</style>
