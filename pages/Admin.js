28b599fe7e0e9b06b0ecca37d8b491e1c940c22d
iwilson <iwilson@oceusnetworks.com>
2020-01-22 09:38:50 -0600

    Added button styles. Colors and select styling may change

--- pages/Admin.js
+++ pages/Admin.js
@@ -163,7 +163,50 @@ export default class Admin extends React.Component {
         <h1>Yet To Vote</h1>
         {this.getUserRow(this.state.users)}
         <style jsx>{`
+          h1 {
 
+          }
+
+          form {
+
+          }
+
+          input {
+
+          }
+
+          button {
+            display: inline-block;
+            border: none;
+            padding: 1rem 2rem;
+            margin: 0;
+            text-decoration: none;
+            background: #0069ed;
+            color: #ffffff;
+            font-family: sans-serif;
+            font-size: 1rem;
+            line-height: 1;
+            cursor: pointer;
+            text-align: center;
+            transition: background 250ms ease-in-out, transform 150ms ease;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+          }
+
+          button:hover,
+          button:focus {
+            background: #0053ba;
+          }
+
+          button:focus {
+            outline: 1px solid #fff;
+            outline-offset: -4px;
+          }
+
+          button:active {
+            transform: scale(0.99);
+          }
+          
         `}</style>
       </Container>
     );
--- pages/Confirm.js
+++ pages/Confirm.js
@@ -99,7 +99,41 @@ export default class Confirm extends React.Component {
           <button name="no" type="button" onClick={this.handleClick.bind(this)}>No</button>
         </Column>
         <style jsx>{`
+          h1 {
 
+          }
+
+          button {
+            display: inline-block;
+            border: none;
+            padding: 1rem 2rem;
+            margin: 0;
+            text-decoration: none;
+            background: #0069ed;
+            color: #ffffff;
+            font-family: sans-serif;
+            font-size: 1rem;
+            line-height: 1;
+            cursor: pointer;
+            text-align: center;
+            transition: background 250ms ease-in-out, transform 150ms ease;
+            -webkit-appearance: none;
+            -moz-appearance: none;
+          }
+
+          button:hover,
+          button:focus {
+            background: #0053ba;
+          }
+
+          button:focus {
+            outline: 1px solid #fff;
+            outline-offset: -4px;
+          }
+
+          button:active {
+            transform: scale(0.99);
+          }
         `}</style>
       </Container>
     );
