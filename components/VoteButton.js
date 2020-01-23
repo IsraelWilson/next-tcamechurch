4614e611911edacd88c8899e06091c82dc456bb3
Israel Wilson <israelsjwilson@gmail.com>
2020-01-23 17:35:43 -0600

    Styling checkboxes in VoteButton

--- components/VoteButton.js
+++ components/VoteButton.js
@@ -1,18 +1,58 @@
 export default function VoteButton(props) {
   return (
-    <>
-      <div>
-        <label>{props.candidate.name}</label>
-        <input name={props.candidate.name} type="checkbox" onChange={props.handleChange}/>
-      </div>
+    <label>
+      {props.candidate.name}
+      <input name={props.candidate.name} type="checkbox" onChange={props.handleChange}/>
+      <span className="checkmark"></span>
       <style jsx>{`
-        div {
-          display: flex;
-          justify-content: space-between;
-          padding-right: 2rem;
+        label {
+          position: absolute;
+          opacity: 0;
+          cursor: pointer;
+          height: 0;
+          width: 0;
+        }
+
+        .checkmark {
+          position: absolute;
+          top: 0;
+          left: 0;
+          height: 25px;
+          width: 25px;
+          background-color: #eee;
+        }
+
+        input:hover ~ .checkmark {
+          background-color: #ccc;
+        }
+
+        input:checked ~ .checkmark {
+          background-color: #2196F3;
+        }
+
+        .checkmark:after {
+          content: "";
+          position: absolute;
+          display: none;
+        }
+
+        input:checked ~ .checkmark:after {
+          display: block;
+        }
+
+        .checkmark:after {
+          left: 9px;
+          top: 5px;
+          width: 5px;
+          height: 10px;
+          border: solid white;
+          border-width: 0 3px 3px 0;
+          -webkit-transform: rotate(45deg);
+          -ms-transform: rotate(45deg);
+          transform: rotate(45deg);
         }
         `}
       </style>
-    </>
+    </label>
   )
 }
