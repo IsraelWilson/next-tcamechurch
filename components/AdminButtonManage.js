df0605ff00097c2759cfd274d736b6ded77ecea6
Israel Wilson <israelsjwilson@gmail.com>
2020-01-22 21:44:53 -0600

    Updating Admin Styles

--- components/AdminButtonManage.js
+++ components/AdminButtonManage.js
@@ -2,7 +2,7 @@ export default function AdminButtonManage(props) {
   return (
     <div>
       <label>{props.candidate.name}</label>
-      <button onClick={props.click}>X</button>
+      <button name={props.candidate.name} onClick={props.click}>X</button>
       <style jsx>{`
         div {
           display: flex;
